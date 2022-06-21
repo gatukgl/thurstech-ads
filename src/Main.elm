7f6488b7f9941f61cc593a0232cb699ab46fe931
Gatuk S. Chattanon <gatukgl@gmail.com>
2022-06-21 21:26:10 +0700

    Add more speaker info in form

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (Msg(..), Speaker, init, main, update, view)
 
 import Browser
-import Html exposing (Html, div, input)
+import Html exposing (Html, div, h1, h2, input, text)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 
@@ -69,8 +69,18 @@ update msg speaker =
 view : Speaker -> Html Msg
 view speaker =
     div []
-        [ input [ placeholder "Speaker name", value speaker.name, onInput Name ] []
-        , input [ placeholder "Job title", value speaker.title, onInput Title ] []
-        , input [ placeholder "Company", value speaker.company, onInput Company ] []
-        , input [ placeholder "Image", value speaker.image, onInput Image ] []
+        [ div []
+            [ h2 [] [ text "Speaker 1" ]
+            , input [ placeholder "Speaker name", value speaker.name, onInput Name ] []
+            , input [ placeholder "Job title", value speaker.title, onInput Title ] []
+            , input [ placeholder "Company", value speaker.company, onInput Company ] []
+            , input [ placeholder "Image", value speaker.image, onInput Image ] []
+            ]
+        , div []
+            [ h2 [] [ text "Speaker 2" ]
+            , input [ placeholder "Speaker name", value speaker.name, onInput Name ] []
+            , input [ placeholder "Job title", value speaker.title, onInput Title ] []
+            , input [ placeholder "Company", value speaker.company, onInput Company ] []
+            , input [ placeholder "Image", value speaker.image, onInput Image ] []
+            ]
         ]
